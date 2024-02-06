3f6ffb34494dcbcc642579b039789bf921b61a41
David Kypuros <davidkypuros@gmail.com>
2024-02-06 07:33:34 -0600

    1

--- src-utility-service/utility-wget-startup.sh
+++ src-utility-service/utility-wget-startup.sh
@@ -5,7 +5,7 @@ DOWNLOAD_PATH="/app/model_repository_zip_files/simple.zip"
 
 # Ensure the script does not attempt operations requiring root privileges
 # Download the file
-if wget -O "${DOWNLOAD_PATH}" https://github.com/drcoopertbbt/mwc-ngc-simulator/raw/main/model_repository/simple.zip; then
+if wget -O "${DOWNLOAD_PATH}" https://github.com/drcoopertbbt/mwc-2024-ngc-models-download-test/raw/main/simple.zip; then
   echo "Download completed successfully."
   # Unzip the file into the model_repository directory
   unzip "${DOWNLOAD_PATH}" -d /app/model_repository
